9400486e4d901f4ffef8062f686bd0cf0a0024f8
Jeremy Paden <jeremy@padenco.com>
2025-10-15 04:40:38 +0000

    chore(migrations): remove active migration files (archived in server/migrations/legacy)

--- server/migrations/knex-migrations/20251008_create_example_table.js
+++
@@ -1,3 +0,0 @@
-// No-op knex migration stub: archived. Fresh DB schema is created by server/db.ts initialization.
-exports.up = async function() { return Promise.resolve(); };
-exports.down = async function() { return Promise.resolve(); };
